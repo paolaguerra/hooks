809e3c5c1d316e6098754aac428162cabb83c8e1
paolaguerra <paolaguerraph@gmail.com>
2023-05-15 14:57:06 -0400

    Pruebas al AppRouter

---
+++ src/test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { MainApp } from "../../09-useContext/MainApp";
+
+describe('Pruenas en <MainApp />', () => { 
+
+    test('debe de mostrar el HomePage', () => { 
+
+        render(
+            <MemoryRouter>
+                <MainApp />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText('HomePage')).toBeTruthy();
+
+     });
+
+     test('debe de mostrar el LoginPage', () => { 
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <MainApp />
+            </MemoryRouter>
+        );
+
+        expect(screen.getByText('LoginPage')).toBeTruthy();
+
+     });
+
+ });
